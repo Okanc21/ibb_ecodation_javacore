740dfbe08362b9f86755e5ed610d0eb556c649d4
Adın Soyadın <email@example.com>
2025-02-12 15:28:56 +0300

    variables

--- src/main/java/com/okancelik/_1_part_javatypes/_02_Variables.java
+++ src/main/java/com/okancelik/_1_part_javatypes/_02_Variables.java
@@ -1,7 +1,16 @@
 package com.okancelik._1_part_javatypes;
 
-public class _02_JavaPrimitiveTypes {
+public class _02_Variables {
     public static void main(String[] args) {
+        //veri değişken isimlerini yazarken;
+        /*
+        veri isimlerndirmeleri;
+        1-) isim, sıfat veya zamir kullanmamız lazım
+        2-) _ veya $ ile başlayabilirsiniz
+        3-) sayı ile bitebilir
+        4-) sayı ile başlanmaz
+        5-)_veya$ dışşında özel  simgelerler başlanmaz.
+        */
 
         // Primitive Tyrpes : 8 tanedir. Null değer alamazsınız.
         // tam sayılar(4) byte, int, short, long
---
+++ src/main/java/com/okancelik/_1_part_javatypes/_03_JavaPrimitiveTypes.java
@@ -0,0 +1,16 @@
+package com.okancelik._1_part_javatypes;
+
+public class _03_JavaPrimitiveTypes {
+    public static void main(String[] args) {
+        //veri değişken isimlerini yazarken;
+        /*
+        veri isimlerndirmeleri;
+        1-) isim, sıfat veya zamir kullanmamız lazım
+        2-) _ veya $ ile başlayabilirsiniz
+        3-) sayı ile bitebilir
+        4-) sayı ile başlanmaz
+        5-)_veya$ dışşında özel  simgelerler başlanmaz.
+        */
+
+    }
+}
