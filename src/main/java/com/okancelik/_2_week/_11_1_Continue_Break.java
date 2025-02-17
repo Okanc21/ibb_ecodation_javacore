ccea64b897fc68193a9be162253bd02c89bea913
Adın Soyadın <email@example.com>
2025-02-17 22:22:10 +0300

    example

---
+++ src/main/java/com/okancelik/_2_week/_11_1_Continue_Break.java
@@ -0,0 +1,11 @@
+package com.okancelik._2_week;
+
+public class _11_1_Continue_Break {
+
+
+
+
+
+
+
+}
--- src/main/java/com/okancelik/_2_week/week2_Examples_1.java
+++ src/main/java/com/okancelik/_2_week/week2_Examples_1.java
@@ -11,9 +11,16 @@ public class week2_Examples_1 {
 
         int number = klavye.nextInt();
         // validation
-        // 1- kullanıcı  pozitif bir sayı girmezse
+        // 1- kullanıcı  pozitif bir sayı girmezse (math.abs) , (negatif bir sayı ile çarp)
         // 2- kullanıcı harf girerse
         // 3- sıfır (çift)
+        if(number<0){
+            System.out.println("negatif bir sayı girdiniz");
+            //number=math.abs(number); // mutlak değeri de alınabilir.
+            number=number*(-1);
+        }
+
+
         if(number%2==0){
             System.out.println(number+ " sayı çift sayıdır");
 
