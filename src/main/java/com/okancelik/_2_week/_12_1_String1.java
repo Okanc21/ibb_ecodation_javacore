33b45ce40be43518ef9e7c63e0cc62b3e79a3448
Adın Soyadın <email@example.com>
2025-02-18 13:51:30 +0300

    example

--- src/main/java/com/okancelik/_2_week/_12_1_String1.java
+++ src/main/java/com/okancelik/_2_week/_12_1_String1.java
@@ -62,6 +62,38 @@ public class _12_1_String1 {
             System.out.println("Verdiğiniz kelime en fazla: "+vocabulary3.length()+ " karakter vardır");
         }
 
+        //Contains
+        System.out.println("Geçiyor mu?"+vocabulary1.contains("Java"));
+
+        // Split
+
+        // Replace
+        vocabulary1=vocabulary1.replace("Java","Java 2025");
+        //System.out.println("Değiştir: "+vocabulary1.replace("Java","Java 2025"));
+        System.out.println(vocabulary1);
+
+        // Birleştirme
+        String concatenation=vocabulary1+vocabulary2+vocabulary3;
+        System.out.println(concatenation);
+        // Birleştirme (Concat)
+        System.out.println(vocabulary1.concat(" jsp").concat(" jsf"));
+
+        // Birleştirme (StringBuilder)
+        StringBuilder stringBuilder= new StringBuilder();
+        stringBuilder.append(vocabulary1).append(" jsp").append(" jsf");
+        String dataToString1= stringBuilder.toString();
+        System.out.println(dataToString1);
+
+        // Birleştirme (StringBuffer)
+        StringBuffer stringBuffer= new StringBuffer();
+        stringBuffer.append(vocabulary1).append(" jsp").append(" jsf");
+        String dataToString2= stringBuffer.toString();
+        System.out.println(dataToString2);
+
+        // Hash code
+        System.out.println(vocabulary1.hashCode());
+        System.out.println(vocabulary2.hashCode());
+        System.out.println(vocabulary3.hashCode());
 
     }
 
