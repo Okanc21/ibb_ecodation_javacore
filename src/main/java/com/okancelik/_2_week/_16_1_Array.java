0135964e85f6e4f9490cb78285ab136f02c89d5c
Adın Soyadın <email@example.com>
2025-02-19 18:11:05 +0300

    Array

--- src/main/java/com/okancelik/_2_week/_16_1_Array.java
+++ src/main/java/com/okancelik/_2_week/_16_1_Array.java
@@ -23,10 +23,25 @@ public class _16_1_Array {
         return city;
 
     }
+    public static String[] arrayMethod2() throws ArrayIndexOutOfBoundsException {
 
-    public static void arrayMethod2() {
+        //String [] city = new String[6]; // eleman sayısı 6
 
-    String[] city = arrayMethod1();
+        //String dizisi
+        String[] city = {"Diyarbakır", "İstanbul","İzmir","null","Ankara","Yalova"};
+
+        // System.out.println(city);
+        // System.out.println("eleman sayısı: " +city.length);
+        // System.out.println(city[0]);
+        // System.out.println(city[6-1]);
+        //  System.out.println(city[4]);// null değer döndürür.
+        //System.out.println("Son eleman: " + city[city.length - 1]);
+        return city;
+
+    }
+    public static void arrayMethod3() {
+
+    String[] city = arrayMethod2();
 
     // for each
         for(String temp : city){
@@ -41,9 +56,11 @@ public class _16_1_Array {
     //psvm
     public static void main(String[] args) {
 
-        //arrayMethod1();
+        // arrayMethod1();
 
-        arrayMethod2();
+        //arrayMethod2();
+        arrayMethod3();
+
         // methot2 ile methot1 içindeki  dizi işlemlerini bu
         // kullanım sayesinde ekrana çıktı olarak verebiliyoruz.
 
